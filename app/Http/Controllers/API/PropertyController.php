6873fad6344792955e7a0fa527d10f1ae462b717
Ming <ming@zookal.com>
2020-11-29 15:05:20 +1100

    Create property API

---
+++ app/Http/Controllers/API/PropertyController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Property;
+use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Validator;
+
+class PropertyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return Property::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $validator = \Validator::make($data, [
+            'suburb' => 'required',
+            'state' => 'required',
+            'country' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $newProperty = new Property;
+        $newProperty->suburb = $data['suburb'];
+        $newProperty->state = $data['state'];
+        $newProperty->country = $data['country'];
+        $newProperty->save();
+
+        return $newProperty;
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Property  $property
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Property $property)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Property  $property
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Property $property)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Property  $property
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Property $property)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PropertyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$CURRENT_VERSION = '2020-11';
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get("/$CURRENT_VERSION/properties", [PropertyController::class, 'index']);
+Route::post("/$CURRENT_VERSION/properties", [PropertyController::class, 'store']);
